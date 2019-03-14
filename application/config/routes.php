24c4bd9af0ba75ce366336c553fe633c07063138
wahyu firmansyah <muhammadwahyufirmansyah@gmail.com>
2019-03-14 17:15:36 +0700

    backup

--- application/config/routes.php
+++ application/config/routes.php
@@ -1,6 +1,10 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-$route['default_controller'] = 'welcome';
+
+$route['news']='news';
+// $route['default_controller'] = 'welcome';
+$route['default_controller'] = 'halaman/view';
+$route['(:any)'] = 'halaman/view/$1';
 $route['404_override'] = '';
 $route['translate_uri_dashes'] = FALSE;
